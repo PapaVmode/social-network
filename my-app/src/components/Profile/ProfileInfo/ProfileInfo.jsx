2510d2ce94eee907e2e69feb009d649091c2ee75
papavmode <papavmode@gmail.com>
2020-05-13 15:16:45 +0300

    поправки

--- my-app/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ my-app/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -9,7 +9,7 @@ const ProfileInfo = (props) => {
     return <Preloader />
   }
   return <div>
-    <div className={s.pictureContent}><img src="https://www.w3schools.com/howto/img_snow_wide.jpg" /></div>
+    <div className={s.pictureContent}><img src="https://www.w3schools.com/howto/img_snow_wide.jpg"  /></div>
     {props.profile.photos.large ? <div className={s.avatar}><img src={props.profile.photos.large} /></div> : <div className={s.avatar}><img src={userPhoto} /></div> }
     {/* <div className={s.avatar}><img src='http://krsk.sibnovosti.ru/pictures/0506/2048/ispytanie_na_prochnost_ob_yatiya_holoda_i_zov_vysoty.jpg' /></div> */}
   </div>
